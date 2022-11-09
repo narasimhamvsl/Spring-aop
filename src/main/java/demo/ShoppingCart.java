dcbee6f608c9aaf16249357422ce31b3dacaf0cf
narasimhamvsl <narasimhamvsl@gmail.com>
2022-11-09 21:08:38 +0530

    Initial version

---
+++ src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+    public void checkout(String status){
+        //Logging
+        //Authentication & Authorization
+        //Sanitization
+        System.out.println("Check out from shopping cart.");
+    }
+}
